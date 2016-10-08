f71e0571dc474dd8e6def629ac6432c8f62e98e5
Westw00d <westerhout_tom@hotmail.com>
2016-10-08 14:03:46 +0200

    Added Substrings method.

---
+++ Substrings.rb
@@ -0,0 +1,20 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+def substrings(input, dictionary)
+	result = {}
+	inp_spl = input.split(" ")
+	inp_spl.each do |inp|
+		dictionary.each do |word|
+			if match = Regexp.new(word).match(inp.downcase)
+				if result.include?(word)
+					result[word] += 1
+				else
+					result[word] = 1
+				end
+			end
+		end
+	end
+	p result.sort
+end
+
+input = gets.chomp
+substrings(input, dictionary)
